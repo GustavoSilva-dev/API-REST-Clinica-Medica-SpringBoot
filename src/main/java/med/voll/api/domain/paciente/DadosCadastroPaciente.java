1d60e7cf90e59404b4904d0597850d8379bd8eff
Gustavo <gustavinnsilva93@gmail.com>
2026-07-08 12:04:05 -0300

    Aplicando testes unitários

--- src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -1,9 +1,7 @@
 package med.voll.api.domain.paciente;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.paciente.endereco.DadosEnderecoPaciente;
 
@@ -23,8 +21,6 @@ public record DadosCadastroPaciente(
         @Pattern(regexp = "\\d{11}")
         String cpf,
 
-        @NotNull
-        @Valid
         DadosEnderecoPaciente endereco
 ) {
 }
